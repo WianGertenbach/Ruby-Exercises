c5dd50ac290d1cb59d561db0238335612e72fbf6
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2025-01-19 15:44:56 +0200

    In this implementation I simply multiply the string by the repeat number

--- repeat_string.rb
+++ repeat_string.rb
@@ -1,8 +1,5 @@
 def repeat_str(string, repeat)
-  repeated_string = ""
-  (1..repeat).each do |val|
-    repeated_string += string
-  end
+  repeated_string = string * repeat
   repeated_string
 end
 
